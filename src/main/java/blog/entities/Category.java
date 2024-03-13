8df545fa4d71d0acc1b637993770b6469591635c
Rishav Raj <rishav3330@gmail.com>
2024-03-13 15:55:51 +0530

    Update Category

--- src/main/java/blog/entities/Category.java
+++ src/main/java/blog/entities/Category.java
@@ -6,6 +6,8 @@ import java.util.List;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import lombok.Getter;
@@ -21,6 +23,7 @@ import lombok.Setter;
 @Entity
 public class Category {
 @Id
+@GeneratedValue(strategy = GenerationType.AUTO)
 private Integer categoryId;
 private String categoryTitle;
 private String categoryDescription;
